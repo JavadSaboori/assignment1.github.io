80e3e26b0997908cfeefc3d8e2f5a0a9fd9fe3dc
Javad Saboori <javad0032@icloud.com>
2024-12-15 16:29:56 +0100

    Add files via upload

--- assignment 1 Javad/script.js
+++ assignment 1 Javad/script.js
@@ -5,4 +5,13 @@ console.log('i am so interesting to learn more about programmering')
 
 let bar=0
 let foo=0
-console.log(bar/foo)
+
+let result= bar/foo
+
+if(isNaN(result)|| bar==0){
+    alert('you can´t divide')
+}
+else{
+    console.log('result:',result)
+}
+

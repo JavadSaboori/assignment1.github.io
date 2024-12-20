dbc10494e9bc77c27f04370793c2b89e8e728488
Javad Saboori <javad0032@icloud.com>
2024-12-20 14:54:57 +0100

    Add files via upload

---
+++ assignment-2,Javad/script/script.js
@@ -0,0 +1,10 @@
+// o=BMW & X=Audi
+
+y=[',','BMW','Audi','BMW','Audi','BMW','Audi','BMW','Audi','BMW','Audi']
+console.log(y);
+
+camp=['o   o    o   o o    o   o     o o    o o      o     o     o  o      o     o   o      o   o']
+
+
+console.log(camp);
+

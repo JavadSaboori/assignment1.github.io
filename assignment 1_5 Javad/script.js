a07e3808783aac4d300d7f6728fcbe9e89c649eb
Javad Saboori <javad0032@icloud.com>
2024-12-20 14:51:57 +0100

    Add files via upload

---
+++ assignment 1_5 Javad/script.js
@@ -0,0 +1,25 @@
+/*
+let age=37
+let vip= important
+
+let vip1= 'let in'
+
+if(inputText='vip'){
+    console.log('you are welcome my best customer ')
+}  else{
+
+     console.log('not allowed')
+    }
+    */
+    let x= 17
+    if(x=17)
+    console.log('Access granled');
+
+    else if ('>=18');
+
+    else('Access denied')
+
+    
+
+
+
